92f321310b42046d52887ab4e34993bdd2858917
jamiul <jamiulewu24@gmail.com>
2022-03-23 15:10:09 +0600

    Laravel JWT Auth and vue integration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:8', 'confirmed']
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required'],
+            'password' => ['required']
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response([
+                'message' => 'Invalid credentials!'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        $cookie = cookie('jwt', $token, 60 * 24); // 1 day
+
+        return response([
+            'message' => $token
+        ])->withCookie($cookie);
+    }
+
+    public function user()
+    {
+        return Auth::user();
+    }
+
+    public function users()
+    {
+        return User::all();
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        $cookie = Cookie::forget('jwt');
+
+        return response([
+            'message' => 'Success'
+        ])->withCookie($cookie);
+    }
+}
